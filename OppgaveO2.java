667de7f5285eb521433cc9352a4e5b5f9cdec413
foleidee <49064462+foleidee@users.noreply.github.com>
2026-09-07 10:45:25 +0200

    ferdig

--- OppgaveO2.java
+++ OppgaveO2.java
@@ -1,38 +1,30 @@
 import java.util.Scanner;
 
-public class Oppgave O2 {
+public class OppgaveO2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-      
-        for (int student = 1; student <= 10; student++) {
-            System.out.print("Poengsum for student " + student + ": ");
-            int poeng = in.nextInt();
+        System.out.print("Skriv inn poengsum: ");
+        int poeng = in.nextInt();
 
-           
-            while (poeng < 0 || poeng > 100) {
-                System.out.println("Feil! Poengsummen må være mellom 0 og 100.");
-                System.out.print("Skriv inn poengsummen på nytt: ");
-                poeng = in.nextInt();
-            }
+        while (poeng < 0 || poeng > 100) {
+            System.out.println("Feil! Poengsummen må være mellom 0 og 100.");
+            System.out.print("Skriv inn poengsum på nytt: ");
+            poeng = in.nextInt();
+        }
 
-            char karakter;
-
-            if (poeng >= 90) {
-                karakter = 'A';
-            } else if (poeng >= 80) {
-                karakter = 'B';
-            } else if (poeng >= 60) {
-                karakter = 'C';
-            } else if (poeng >= 50) {
-                karakter = 'D';
-            } else if (poeng >= 40) {
-                karakter = 'E';
-            } else {
-                karakter = 'F';
-            }
-
-            System.out.println("Karakter: " + karakter);
+        if (poeng >= 90) {
+            System.out.println("Karakter: A");
+        } else if (poeng >= 80) {
+            System.out.println("Karakter: B");
+        } else if (poeng >= 60) {
+            System.out.println("Karakter: C");
+        } else if (poeng >= 50) {
+            System.out.println("Karakter: D");
+        } else if (poeng >= 40) {
+            System.out.println("Karakter: E");
+        } else {
+            System.out.println("Karakter: F");
         }
 
         in.close();
